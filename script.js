a1ac95f59fa092fb4777064391e0104bee16c67c
wasencroll <alexandercroll@icloud.com>
2018-12-02 03:17:55 +0100

    hover on mobile

--- script.js
+++ script.js
@@ -64,8 +64,4 @@ $(document).ready(function() {
         console.log(width);
     });
 
-    document.addEventListener('touchstart', function addtouchclass(e){ // first time user touches the screen
-        document.documentElement.classList.add('can-touch') // add "can-touch" class to document root using classList API
-        document.removeEventListener('touchstart', addtouchclass, false) // de-register touchstart event
-    }, false)
 });
